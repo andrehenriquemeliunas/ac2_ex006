5b733e7c632fb43f779fc76546977d6dc3d91dab
Andre Henrique <andremeliunas@hotmail.com>
2022-05-16 07:46:46 -0300

    Update ex006.java

--- ex006.java
+++ ex006.java
@@ -8,20 +8,27 @@ public class ex006{
             System.out.println("Em quem irá votar? Digite o número correspondente.(1-PT | 2-PDT | 3-PL | 4-PSDB | 5-NULO | 6-BRANCO)");//Mostrando as opções para os usuários
             voto = s.nextInt();//Pega a resposta do usuário e armazena na variável voto
 
-            if(voto == 1 ){//Consdição SE o voto for 1 
-                pt = pt + 1;//Soma var pt mais 1
-            }else if(voto == 2 ){//Consdição SE o voto for 2
-                pdt = pdt + 1;//Soma var pdt mais 1
-            }else if(voto == 3 ){//Consdição SE o voto for 3 
-                pl = pl + 1;//Soma var pl mais 1
-            }else if(voto == 4 ){//Consdição SE o voto for 4
-                psdb = psdb + 1;//Soma var psdb mais 1
-            }else if(voto == 5 ){//Consdição SE o voto for 5 
-                nulo = nulo + 1;//Soma var nulo mais 1
-            }else if(voto == 6 ){//Consdição SE o voto for 6 
-                branco = branco + 1;//Soma var branco mais 1
-            }else{//Condição final
-                System.out.println("Opção de voto inválido!");//Fala ao usuário que a opção de voto é inválida
+            switch(voto){
+                case 1://Caso o voto seja 1
+                    pt = pt + 1;//Soma var pt mais 1
+                    break;
+                case 2://Caso o voto seja 2
+                    pdt = pdt + 1;//Soma var pdt mais 1
+                    break;
+                case 3://Caso o voto seja 3
+                    pl = pl + 1;//Soma var pl mais 1
+                    break;
+                case 4://Caso o voto seja 4
+                    psdb = psdb + 1;//Soma var psdb mais 1
+                    break;
+                case 5://Caso o voto seja 5
+                    nulo = nulo + 1;//Soma var nulo mais 1
+                    break;
+                case 6://Caso o voto seja 6
+                    branco = branco + 1;//Soma var branco mais 1
+                    break;
+                default://Caso o usuáio digite algo fora do esperado
+                    System.out.println("Opção de voto inválido!");//Fala ao usuário que a opção de voto é inválida
             }
         }
         System.out.println("Votos no PT: " + pt);//Mostra ao usuáio os votos no pt
